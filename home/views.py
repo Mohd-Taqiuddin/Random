51ccb4354ed5514c43979f7553bcde4c32ce1c90
Mohd Taqiuddin <taqiuddin12345@gmail.com>
2022-01-08 02:23:37 +0530

    templates

--- home/views.py
+++ home/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from . import urls
 
-# Create your views here.
+def index(request):
+    return HttpResponse("Hello, world. You're at the index.")
