1d661f97460dfd09e271d0eb2fcddbd6f8cf6803
Mohd Taqiuddin <taqiuddin12345@gmail.com>
2022-01-08 17:21:04 +0530

    working on db/added2mysql

--- home/models.py
+++ home/models.py
@@ -2,3 +2,10 @@ from django.db import models
 
 # Create your models here.
 
+class Event(models.Model):
+    event_name = models.CharField(max_length=50)
+    date = models.DateField()
+    time = models.TimeField()
+    location = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="uploads/")
+    is_liked = models.BooleanField(default=False)
