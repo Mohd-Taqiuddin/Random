2806bec1d40b7fe47f453366db89ba916fa4ab01
Mohd Taqiuddin <taqiuddin12345@gmail.com>
2022-01-08 23:02:27 +0530

    working on image

--- Eventbrite/settings.py
+++ Eventbrite/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -124,7 +125,14 @@ DATE_INPUT_FORMATS = ['%d-%m-%Y']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIR = (
+    os.path.join(BASE_DIR, 'home/static'),
+)
+
+# MEDIA_URL = 'home/static/'
+# MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'Eventbrite')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- home/views.py
+++ home/views.py
@@ -1,9 +1,28 @@
+from django.core.checks import messages
 from django.http import HttpResponse
+
+from .models import Event
 from . import urls
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 def index(request):
-    return render(request, 'index.html')
+    events = Event.objects.all()
+    context = {'events':events}
+    return render(request, 'index.html',context)
+
 
 def addEvent(request):
+    if request.method == "POST":
+        event = Event()
+        event.email = request.POST.get('email')
+        event.password = request.POST.get('password')
+        event.event_name = request.POST.get('event_name')
+        event.date = request.POST.get('date')
+        event.time = request.POST.get('time')
+        event.location = request.POST.get('location')
+        event.image = request.POST.get('image')
+
+        event.save()
+        # messages.success(request, "Event Added Successfully!")
+        return redirect('/')
     return render(request, 'add_event.html')
